c36740d0afd114120ccc11d4a4d049c462c3e486
natekratchman <natecrashman@gmail.com>
2014-11-07 10:56:56 -0500

    started oauth stuff

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :spotify, "ce33f36675d04c8eb33a81ce4967a501", "01ba7ef1a474410dba5d939f95b5681a", scope: 'playlist-read-private streaming user-library-read user-read-private'
+  provider :spotify, "CLIENT_ID", "CLIENT_SECRET", scope: 'playlist-read-private streaming user-library-read user-read-private'
 end
